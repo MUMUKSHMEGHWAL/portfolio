5e01845a80fc0d6e8ee68d2d83faaaeb187321cf
MUMUKSHMEGHWAL <mumukshmeghwal@gmail.com>
2025-05-03 14:46:39 +0530

    updating .yml to fix deployment error

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig({
-  base: "/portfolio/",
+  base: "/portfolio/dist/public",
   plugins: [
     react(),
     runtimeErrorOverlay(),
